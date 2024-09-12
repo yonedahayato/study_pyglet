289838a8a16f3157e1cbd049c4d24a76e3a12d83
yoneda <yhayato1320@gmail.com>
2024-09-12 21:30:41 +0900

    kivy でのゲーム作成一段落

---
+++ game_prog_nyumon/game_prog_nyumon/chapter2/player3.py
@@ -0,0 +1,54 @@
+import sys
+from pathlib import Path
+from pyglet.window.key import (
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+)
+
+
+GAME_DIR = str(Path(__file__).resolve().parents[1])
+sys.path.append(GAME_DIR)
+
+from game import (
+    add, 
+    image, 
+    run,
+    Mover,
+    key,
+)
+
+chicken_image_file = "chicken.png"
+image_player = image(chicken_image_file)
+
+def player(p: Mover) -> None:
+    """
+    自機の処理
+    """
+
+    v: float = 0.01
+
+    if key(LEFT):
+        p.x -= v
+
+    if key(RIGHT):
+        p.x += v
+
+    if key(UP):
+        p.y += v
+
+    if key(DOWN):
+        p.y -= v
+
+def start():
+    """
+    ゲームの開始処理
+    """
+    print("start func")
+    add(player, image_player, size=0.09, 
+        x=0, y=0, vx=0, vy=0)
+
+if __name__ == "__main__":
+    print("player1.py")
+    run(start, 1280, 720)
--- game_prog_nyumon/pyglet_to_kivy/game.py
+++ game_prog_nyumon/pyglet_to_kivy/game.py
@@ -1,7 +1,10 @@
+import kivy
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, 
+    ReferenceListProperty, 
+    ObjectProperty
 )
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
@@ -9,6 +12,7 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 
 from pathlib import Path
+from random import randint
 
 # from kivy.lang import Builder
 # Builder.load_file('./pyglet_to_kivy/player.kv')
@@ -26,47 +30,152 @@ FPS: float = 60.0
 # RESOURCE_DIR = Path(__file__).resolve().parents[1] / "resource" / "download_pipoya"
 RESOURCE_DIR = Path(__file__).resolve().parents[1] / "resource"
 
-# class PongBall(Widget):
-class PongBall(Image):
+class PongBall(Widget):
+    """
+    ボールの物体
+    """
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    v_operation = 20
+
+    def move(self, operation: str = ""):
+        """
+        等速直線運動の場合の動く処理
+        """
+        if operation == "up":
+            self.pos = [self.pos[0], self.pos[1] + self.v_operation]
+        elif operation == "down":
+            self.pos = [self.pos[0], self.pos[1] - self.v_operation]
+        elif operation == "left":
+            self.pos = [self.pos[0] - self.v_operation, self.pos[1]]
+        elif operation == "right":
+            self.pos = [self.pos[0] + self.v_operation, self.pos[1]]
+        else:
+            self.pos: kivy.properties.ObservableReferenceList = Vector(*self.velocity) + self.pos
+
+class CharacterBall(Image):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.pos: kivy.properties.ObservableReferenceList = Vector(*self.velocity) + self.pos
 
 class PlayerGame(Widget):
     """
     ゲームの実行を管理
     """
     ball = ObjectProperty(None)
+    character_ball = ObjectProperty(None)
+    cho_cho = ObjectProperty(None)
 
-    def set_image_path(self, image_name: str = "chicken.png"):
+    keys = []
+
+    def set_image_path(self,
+                       object: ObjectProperty,
+                       image_name: str = "chicken.png"):
         """
         画像のパスを設定する
         """
         image_path = RESOURCE_DIR / image_name
-        self.ball.source = str(image_path)
+        object.source = str(image_path)
+        return object
 
-    def serve_ball(self):
-        print("serve_ball func")
-        print(f"center: {self.center}")
+    def set_images_path(self):
+        """
+        画像の設定
+        """
+        self.character_ball = self.set_image_path(self.character_ball, "by_max.png")
+        self.cho_cho = self.set_image_path(self.cho_cho, "chocho.webp")
 
-        # self.ball.center = self.center
-        self.ball.center = [0, Window.size[1] / 2]
-        self.ball.velocity = Vector(4, 0).rotate(0)
+    def serve_ball(self, ball):
+        ball.center = self.center
+        ball.velocity = Vector(4, 0).rotate(randint(0, 360))
 
-    def update(self, dt):
-        self.ball.move()
+        return ball
+
+    def serve_balls(self):
+        self.ball.center = self.center
+        # self.ball.center = [0, Window.size[1] / 2]
+
+        # self.ball.velocity = Vector(4, 0).rotate(0)
+        # self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = (0, 0)
+
+        self.character_ball = self.serve_ball(self.character_ball)
+        self.cho_cho = self.serve_ball(self.cho_cho)
+
+    def updata_each_ball(self, ball):
+        ball.move()
+
+        # bounce off top and bottom
+        if (ball.y < 0) or (ball.top > self.height):
+            ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (ball.x < 0) or (ball.right > self.width):
+            ball.velocity_x *= -1
+
+        return ball
+
+    def update(self, dt) -> None:
+        """
+        状態を更新する
+        """
+        if len(self.keys) != 0:
+            key = self.keys.pop()
+            print(f"update key: {key}")
+
+            if key in ["up", "down", "left", "right"]:
+                self.ball.move(operation=key)
+
+        self.ball = self.updata_each_ball(self.ball)
+        self.character_ball = self.updata_each_ball(self.character_ball)
+        self.cho_cho = self.updata_each_ball(self.cho_cho)
+
+    def __init__(self, **kwargs):
+        super(PlayerGame, self).__init__(**kwargs)
+
+        self._keyboard = Window.request_keyboard(
+            self._keyboard_closed, self, 'text')
+
+        if self._keyboard.widget:
+            # If it exists, this widget is a VKeyboard object which you can use
+            # to change the keyboard layout.
+            pass
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        print('My keyboard have been closed!')
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        print('The key', keycode, 'have been pressed')
+        print(' - text is %r' % text)
+        print(' - modifiers are %r' % modifiers)
+
+        # Keycode is composed of an integer + a string
+        # If we hit escape, release the keyboard
+        if keycode[1] == 'escape':
+            keyboard.release()
+
+        if keycode[1] in ["up", "down", "left", "right"]:
+            self.keys.append(keycode[1])
+
+        # Return True to accept the key. Otherwise, it will be used by
+        # the system.
+        return True
 
 class PlayerApp(App):
     def build(self):
         game = PlayerGame()
-        game.set_image_path("by_max.png")
-        game.serve_ball()
+        game.set_images_path()
+        game.serve_balls()
         Clock.schedule_interval(game.update, 1.0/FPS)
         return game
-
 
 if __name__ == '__main__':
     PlayerApp().run()
